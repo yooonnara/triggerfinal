6e2a8e450771d3639e6c049c420cfa1304402dec
user <user@DESKTOP-M8CTEFU>
2023-07-28 14:32:22 +0900

    나빈 전자결재 작성중

--- src/main/java/com/tr/join/edsm/model/dao/edsmDao.java
+++ src/main/java/com/tr/join/edsm/model/dao/edsmDao.java
@@ -1,5 +1,7 @@
 package com.tr.join.edsm.model.dao;
 
+
+
 import org.apache.ibatis.session.SqlSession;
 
 import com.tr.join.employee.model.vo.Employee;
--- src/main/java/com/tr/join/edsm/model/dao/edsmDaoImpl.java
+++ src/main/java/com/tr/join/edsm/model/dao/edsmDaoImpl.java
@@ -1,9 +1,10 @@
 package com.tr.join.edsm.model.dao;
 
 import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Repository;
 
 import com.tr.join.employee.model.vo.Employee;
-
+@Repository
 public class edsmDaoImpl implements edsmDao {
 
 	@Override
