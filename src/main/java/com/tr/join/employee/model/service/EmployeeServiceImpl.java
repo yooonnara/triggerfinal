5df42068c767eebaa160af569ca5c7590a00a0e9
DESKTOP-C1T83CR\USER <yooonnara@gmail.com>
2023-07-28 14:19:16 +0900

    nara : @Service 어노테이션 수정

--- src/main/java/com/tr/join/employee/model/service/EmployeeServiceImpl.java
+++ src/main/java/com/tr/join/employee/model/service/EmployeeServiceImpl.java
@@ -2,10 +2,12 @@ package com.tr.join.employee.model.service;
 
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.tr.join.employee.model.dao.EmployeeDao;
 import com.tr.join.employee.model.vo.Employee;
 
+@Service
 public class EmployeeServiceImpl implements EmployeeService {
 	
 	@Autowired
